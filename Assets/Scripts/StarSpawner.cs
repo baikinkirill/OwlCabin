532ba7e4d2dca5c89148b19390803056b992256e
Danila <dakfadeev@gmail.com>
2020-11-07 00:44:46 +0300

    180 angle/360 angle

--- Assets/Scripts/StarSpawner.cs
+++ Assets/Scripts/StarSpawner.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class StarSpawner : MonoBehaviour
 {
+    [SerializeField] int StartStarsCount = 5;
     Vector3 startPos;
     [SerializeField] float range = 1f;
     [SerializeField] GameObject sphere;
+    int difficulty = 0;
+
     void Start()
     {
         startPos = gameObject.transform.position;
+        for (int i = 0; i < StartStarsCount; i++)
+        {
+            PlaceStars();
+        }
     }
 
-    void PlaceStars()
+    public void PlaceStars()
     {
-        Ray ray = new Ray(startPos + new Vector3(0, Random.Range(-startPos.y + range, startPos.y + range)), new Vector3(Random.Range(-180, 180), 0, Random.Range(-180, 180)));
+        Ray ray = new Ray(startPos + new Vector3(0, Random.Range(-startPos.y + range, startPos.y + range), 0), new Vector3(Random.Range(-180, 180), 0, Random.Range(difficulty, 180)));
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100))
+        if (Physics.Raycast(ray, out hit, 10f))
         {
-            Debug.Log(hit.collider.tag);
             if (hit.collider.tag == "Cylinder")
             {
-                Debug.DrawRay(ray.origin, hit.point, Color.red);
                 SpawnStars(hit.point, hit.transform.rotation);
             }
         }
@@ -30,5 +35,17 @@ public class StarSpawner : MonoBehaviour
     void SpawnStars(Vector3 pos, Quaternion rot)
     {
         GameObject.Instantiate(sphere, pos, rot);
+    }
+
+    public void ChangeDifficulty()
+    {
+        if (difficulty == 0)
+        {
+            difficulty = -180;
+        }
+        else
+        {
+            difficulty = 0;
+        }
     }
 }
