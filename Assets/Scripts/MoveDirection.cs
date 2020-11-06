29c8b61a9d061e6376dcae4247b559606f79b6a6
Mixail <mixail.korchikov@mail.ru>
2020-11-06 22:04:47 +0300

    movement direction, PlaneChander, star

---
+++ Assets/Scripts/MoveDirection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDirection : MonoBehaviour
+{
+    public Vector3 direction { private set; get; }
+    private Transform transform;
+    private Ray ray;
+    private RaycastHit hit = new RaycastHit();
+    private void Awake()
+    {
+        transform = gameObject.transform;
+    }
+
+    private void Update()
+    {
+        ray = new Ray(transform.position, transform.forward);
+
+        if(Physics.Raycast(ray, out hit) && hit.collider.tag == "Cylinder")
+        {
+            direction = hit.point;
+        }
+    }
+}
