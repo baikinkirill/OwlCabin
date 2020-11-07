0d2a4c23029b9214d2855a865c378de373fca34d
Danila <dakfadeev@gmail.com>
2020-11-07 22:21:28 +0300

    make UI menu

---
+++ Assets/Scripts/CanvasNavigation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasNavigation : MonoBehaviour
+{
+    StarSpawner SS;
+    [SerializeField] Text zone;
+
+    void Start()
+    {
+        SS = GameObject.Find("StarSpawner").GetComponent<StarSpawner>();
+    }
+
+    public void LoadFirstMap()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void LoadSecondMap()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void LoadThirdMap()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void ChangeDifficulty()
+    {
+        SS.ChangeDifficulty();
+        if (zone.text == "Playzone 180°")
+        {
+            zone.text = "Playzone 360°";
+        }
+        else
+        {
+            zone.text = "Playzone 180°";
+        }
+    }
+}
