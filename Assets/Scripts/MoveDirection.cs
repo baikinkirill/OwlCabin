8097e06b9cfddc612dc272d470efffc9ee8a394b
Mixail <mixail.korchikov@mail.ru>
2020-11-07 15:25:00 +0300

    plane rotation

--- Assets/Scripts/MoveDirection.cs
+++ Assets/Scripts/MoveDirection.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class MoveDirection : MonoBehaviour
 {
     public Vector3 direction { private set; get; }
+    public Transform gazeTransform { private set; get; }
     private Transform transform;
     private Ray ray;
     private RaycastHit hit = new RaycastHit();
+    private GameObject gaze;
     private void Awake()
     {
         transform = gameObject.transform;
+        gaze = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        gaze.GetComponent<MeshRenderer>().enabled = false;
+        gaze.GetComponent<SphereCollider>().enabled = false;
+        gazeTransform = gaze.transform;
     }
 
     private void Update()
@@ -21,6 +27,7 @@ public class MoveDirection : MonoBehaviour
         {
             Debug.DrawLine(ray.origin, hit.point);
             direction = hit.point;
+            gaze.transform.position = hit.point;
         }
     }
 }
--- Assets/Scripts/PlaneRotation.cs
+++ Assets/Scripts/PlaneRotation.cs
@@ -7,14 +7,66 @@ public class PlaneRotation : MonoBehaviour
     [SerializeField] private MoveDirection moveDirection;
     [Range(0, 5)]
     [SerializeField] private float speed = 3f;
+    [SerializeField] private Transform plane;
+    [Range(0f, 50f)] [SerializeField] private float ignoreAngle;
+    [SerializeField] private Rigidbody rigidbody;
+    private float angle;
+    private Direction localDirection = Direction.Watting;
+    private bool flightStatus = true;   //Отрицательный пока происходит разворот самолета.
 
     private void FixedUpdate()
     {
+        Vector3 targetDir = moveDirection.direction - gameObject.transform.position;
+        targetDir = new Vector3(targetDir.x, 0, targetDir.z);
+        Vector3 planeDir = plane.position - gameObject.transform.position;
+        planeDir = new Vector3(planeDir.x, 0, planeDir.z);
+        angle = Vector3.SignedAngle(targetDir, planeDir, gameObject.transform.up);
+        
+        if (Mathf.Abs(angle) > ignoreAngle)
+        {
+            if (angle < 0)
+            {
+                plane.localScale = new Vector3(plane.localScale.x, plane.localScale.y, Mathf.Abs(plane.localScale.z) * -1);
+                localDirection = Direction.Right;
+            }
+            else
+            {
+                localDirection = Direction.Left;
+                plane.localScale = new Vector3(plane.localScale.x, plane.localScale.y, Mathf.Abs(plane.localScale.z));
+            }
+        }
+        else
+        {
+            localDirection = Direction.Watting;
+        }
+
+        if(localDirection == Direction.Watting || !flightStatus) { return; }
+
         LookAt(moveDirection.direction);
+    }
+
+    private void Update()
+    {
+        PlaneLookAt();
     }
 
     public void LookAt(Vector3 pos)
     {
         gameObject.transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, Quaternion.LookRotation(pos), speed);
     }
+
+    public void PlaneLookAt()
+    {
+        plane.rotation = Quaternion.Lerp(moveDirection.gazeTransform.rotation, plane.rotation, Time.deltaTime * 600f);
+    }
+
+    enum Direction
+    {
+        Watting,
+        Left,
+        Right,
+        ChangeDirection
+    }
+
+    
 }
