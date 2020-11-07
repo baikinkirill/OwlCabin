67e3e84c9d33e991861ae18ad914a0718bee469e
Danila <dakfadeev@gmail.com>
2020-11-07 17:28:18 +0300

    set up sounds

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -6,15 +6,24 @@ public class Star : MonoBehaviour
 {
     public int starCost { private set; get; }
     StarSpawner SP;
+    AudioSource AS;
+    [SerializeField]AudioClip AC;
 
     private void Start()
     {
         SP = GameObject.Find("StarSpawner").GetComponent<StarSpawner>();
+        AS = gameObject.GetComponent<AudioSource>();
     }
 
     public void onCatch()   //Вызывается, когда самолет хватает звездочку. 
     {
+        AS.PlayOneShot(AC, 0.5f);
         SP.PlaceStars();
+        StartCoroutine("destroy");
+    }
+    IEnumerator destroy()
+    {
+        yield return new WaitForSeconds(AC.length);
         Destroy(gameObject);
     }
 }
